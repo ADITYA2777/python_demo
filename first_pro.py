2b04fc95b56824a1395ec16ee341471efeed9747
ADITYA2777 <ajadityajain01@gmail.com>
2024-01-26 21:00:23 +0530

    python demo

---
+++ first_pro.py
@@ -0,0 +1,19 @@
+
+num1 = float(input("Enter the num1"))
+num2 = float(input("Enter the num2"))
+
+opr = input("Enter operator")
+
+if opr == "+":
+      output= num1+num2
+
+print("your calculation is :",output)
+
+# if opr == "+":
+#     output= num1+num2
+
+#     if opr == "-":
+#     # output= num1-num2
+
+#     if opr == "*":
+#     # output= num1*num2
