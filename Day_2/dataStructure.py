d587ff099b7c7eaa1edc51151eec5e66805b9216
ADITYA2777 <ajadityajain01@gmail.com>
2024-01-27 23:47:57 +0530

    dataStructure

---
+++ Day_2/dataStructure.py
@@ -0,0 +1,19 @@
+my_list = list(["ad","ak","fz"])
+
+list_env= ["add","sub","mut"]
+
+
+print((list_env.append("jai ho")))
+(my_list.append("jai hoooo"))
+print(my_list)
+
+
+# list_env.insert(1,"testing")
+list_env.reverse()
+# list_env.extend("aditya")
+# list_env.sort()
+
+print(list_env)
+
+
+    
