d25570557f97a5a15a6df40c53ff04c373cfa607
ADITYA2777 <ajadityajain01@gmail.com>
2024-01-28 23:43:45 +0530

    fundmental

---
+++ Day_2/Set.py
@@ -0,0 +1,11 @@
+
+set_1 = {1,1,1,1,1,2,2,2,2,4,4,4,55,66,7,40}
+set_2= {33,44,55,5,4,5,5,5,5,77}
+
+# print(set_1.intersection(set_2))
+# print (set_1.union(set_2))
+
+list_of_env = ["ad","ak","ak","aaaa","aa"]
+# print(list_of_env)
+list_of_env = list(set(list_of_env))
+print(list_of_env)
