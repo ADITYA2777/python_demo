3615d8b299305f94b987c674232826075f5f7d10
ADITYA2777 <ajadityajain01@gmail.com>
2024-01-30 22:55:49 +0530

    import lib

--- Day_2/chai_withcode.py
+++ Day_2/chai_withcode.py
@@ -3,3 +3,7 @@ def chai(n):
     print(n)
 
 chai("aditya")
+
+chai_one ="lemon tea"
+chai_two= "ginger tea"
+chai_three = "masal tea"
--- Day_2/import.py
+++ Day_2/import.py
@@ -2,3 +2,4 @@
 from  chai_withcode import chai
 
 chai("akshay")
+
