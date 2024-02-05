784546589b4fdd768156fdbd17a68192b163edff
ADITYA2777 <ajadityajain01@gmail.com>
2024-02-05 23:14:05 +0530

    num in python

--- Day3/number_depth.py
+++ Day3/number_depth.py
@@ -19,3 +19,22 @@ print("your calculation is :",output)
 
 #     if opr == "*":
 #     # output= num1*num2
+
+
+num2 = float(input("Enter the num2"))
+
+opr = input("Enter operator")
+
+if opr == "+":
+      output= num1+num2
+
+print("your calculation is :",output)
+
+# if opr == "+":
+#     output= num1+num2
+
+#     if opr == "-":
+#     # output= num1-num2
+
+#     if opr == "*":
+#     # output= num1*num2
