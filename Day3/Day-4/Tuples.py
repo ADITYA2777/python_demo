b1b884f1dfa077709ee39d1f262a39da220b3659
ADITYA2777 <ajadityajain01@gmail.com>
2024-02-13 23:46:20 +0530

    docker

--- Day3/Day-4/Tuples.py
+++ Day3/Day-4/Tuples.py
@@ -46,3 +46,54 @@
 # 2
 # >>> lng(all_tea)
 # Traceback (most recent call last)
+
+
+
+# tea_chai
+# ('black', 'green', 'oonlong')
+# >>> tea_chai[0]= "Lemon"
+#
+# >>> tea_chai
+# ('black', 'green', 'oonlong')
+# >>> more_tea = ("Herbal","Earl gary","lemon")
+# >>> all_tea = more_tea + tea_chai
+# >>> all_tea
+# ('Herbal', 'Earl gary', 'lemon', 'black', 'green', 'oonlong')
+# >>> all_tea.count("herbal")
+# 0
+# >>> more_tea = ("Herbal","Earl gary","lemon","Herbal")
+# >>> more_tea.count("Herbal")
+# 2
+# >>> lng(all_tea)
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+# NameError: name 'lng' is not defined
+# >>> len(all_tea)
+# 6
+
+# >> tea_chai
+# ('black', 'green', 'oonlong')
+# >>> more_tea = ("Herbal","Earl gary","lemon")
+# >>> all_tea = more_tea + tea_chai
+# >>> all_tea
+# ('Herbal', 'Earl gary', 'lemon', 'black', 'green', 'oonlong')
+# >>> all_tea.count("herbal")
+# 0
+# >>> more_tea = ("Herbal","Earl gary","lemon","Herbal")
+# >>> more_tea.count("Herbal")
+# 2
+# >>> lng(all_tea)
+# Traceback (most recent call last)
+>> tea_chai
+# ('black', 'green', 'oonlong')
+# >>> more_tea = ("Herbal","Earl gary","lemon")
+# >>> all_tea = more_tea + tea_chai
+# >>> all_tea
+# ('Herbal', 'Earl gary', 'lemon', 'black', 'green', 'oonlong')
+# >>> all_tea.count("herbal")
+# 0
+# >>> more_tea = ("Herbal","Earl gary","lemon","Herbal")
+# >>> more_tea.count("Herbal")
+# 2
+# >>> lng(all_tea)
+# Traceback (most recent call last)
