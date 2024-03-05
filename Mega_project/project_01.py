0ef8476c027836f9d49f17c5341d5b58549c77d1
ADITYA2777 <ajadityajain01@gmail.com>
2024-03-05 23:48:06 +0530

    decorate fun

--- Mega_project/project_01.py
+++ Mega_project/project_01.py
@@ -17,3 +17,18 @@ def greet(name, greeting ="hello"):
 
 
     greet("chai", greeting ="adi")
+
+    def greet(name, greeting ="hello"):
+    print(f"{greeting},{name}")
+
+
+    greet("chai", greeting ="adi") 
+
+    lee = ("aditya","akshay")
+
+
+def greet(name, greeting ="hello"):
+    print(f"{greeting},{name}")
+
+
+    greet("chai", greeting ="adi")
