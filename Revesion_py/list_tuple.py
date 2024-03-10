e2ce78cae4301b33392900a553b35561471dfbd1
ADITYA2777 <ajadityajain01@gmail.com>
2024-03-10 23:11:17 +0530

    api in python

--- Revesion_py/list_tuple.py
+++ Revesion_py/list_tuple.py
@@ -13,27 +13,27 @@ for i in list_OF_envs:
     print(list_OF_envs)
 
 
-list_of_num = [1,2,3,-2,-1,4,99,22,44,7]
+# list_of_num = [1,2,3,-2,-1,4,99,22,44,7]
 
-for i in range(len(list_of_num)):
-    for j in  range(len(list_of_num)):
-        if list_of_num[i] > list_of_num[j]:
-            ad=list_of_num[i]
-            list_of_num[i] =list_of_num[j]
-            list_of_num[j]=ad
+# for i in range(len(list_of_num)):
+#     for j in  range(len(list_of_num)):
+#         if list_of_num[i] > list_of_num[j]:
+#             ad=list_of_num[i]
+#             list_of_num[i] =list_of_num[j]
+#             list_of_num[j]=ad
 
-print(list_of_num)
+# print(list_of_num)
 
 
-list_of_num = [1,2,3,-2,-1,4,99,22,44,7]
+# list_of_num = [1,2,3,-2,-1,4,99,22,44,7]
 
-for i in range(len(list_of_num)):
-    for j in  range(len(list_of_num)):
-        if list_of_num[i] > list_of_num[j]:
-            ad=list_of_num[i]
-            list_of_num[i] =list_of_num[j]
-            list_of_num[j]=ad
+# for i in range(len(list_of_num)):
+#     for j in  range(len(list_of_num)):
+#         if list_of_num[i] > list_of_num[j]:
+#             ad=list_of_num[i]
+#             list_of_num[i] =list_of_num[j]
+#             list_of_num[j]=ad
 
-print(list_of_num)
+# print(list_of_num)
         
         
