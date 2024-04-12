b7db58161a7be1316cdc81f1028ff6a38299e9f4
adityajain <ajadityajain@01gmail.com>
2024-04-12 23:59:17 +0530

    python_fun

--- Revesion_py/ultils.py
+++ Revesion_py/ultils.py
@@ -9,3 +9,11 @@ def check_even_odd(num):
 
 
 this is new
+
+def to_uppcase(string_word):
+    return string_word.upper()
+
+def check_even_odd(num):
+    if num % 2 == 0:
+        return True
+    return False
